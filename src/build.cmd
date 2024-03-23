d68ab27846c5aa21e644b7871d57a08f272e7d11
Matthew Justice <mattjustice@hotmail.com>
2024-03-22 20:17:12 -0400

    Add rc file, manifest, and ico (placeholder)

--- src/build.cmd
+++ src/build.cmd
@@ -3,11 +3,13 @@ set OUTPUT_EXE=esnpad.exe
 
 rmdir /s /q %OUTPUT_PATH%
 mkdir %OUTPUT_PATH%
+rc.exe /fo %OUTPUT_PATH%/resources.res resources.rc
 
 cl.exe main.c ^
 /DUNICODE /D_UNICODE /WX /EHsc /Zi ^
 /Fe%OUTPUT_PATH%\%OUTPUT_EXE% /Fo%OUTPUT_PATH%\ /Fd%OUTPUT_PATH%\vc140.pdb ^
-/link /SUBSYSTEM:WINDOWS user32.lib gdi32.lib
+/link /SUBSYSTEM:WINDOWS user32.lib gdi32.lib %OUTPUT_PATH%\resources.res
 
 del %OUTPUT_PATH%\*.obj
 del %OUTPUT_PATH%\*.ilk
+del %OUTPUT_PATH%\*.res
